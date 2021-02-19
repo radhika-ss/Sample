3f364b47c2ef90a70031d6e431c97e427c7c45d3
radhika-ss <radhika.deep5@gmail.com>
2021-02-18 22:19:30 -0500

    Develop branch changes

---
+++ sample/src/sample/Apps.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Apps {
+
+	public static void main(String[] args) {
+		System.out.println("Framework chabnges");
+
+	}
+
+}
---
+++ sample/src/sample/pass.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class pass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pass");
+	}
+
+}
